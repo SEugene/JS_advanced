84d674f1065e03942945560bb060f21b014d4343
sea.05 <sea.05@list.ru>
2022-09-24 16:16:02 +0300

    The third task is fixed. Styles changed. Checkbox for toppings added

--- burgers/burger.js
+++ burgers/burger.js
@@ -2,7 +2,7 @@ class Hamburger {
     constructor(size, stuffing, topping) { 
         this.size = this._selector(size);
         this.stuffing = this._selector(stuffing);
-        this.topping = this._selector(topping);
+        this.topping = this._selectorMany(topping);
 
         this.sizes = {
             'big': {calories: 40, price: 100},
@@ -17,19 +17,32 @@ class Hamburger {
             'spice': {calories: 0, price: 15},
             'mayonnaise': {calories: 5, price: 20},
         };
-
-        //this.burgerPrice = this._calculator('price');
-        //this.burgerCalories = this._calculator('calories');
     }
 
+    // метод выбора для radio
     _selector(item){
         return document.querySelector(`input[name=${item}]:checked`).value;
     }
+
+    // метод выбора для checkbox
+    _selectorMany(item){
+        let itemChoices = [...document.querySelectorAll(`input[name=${item}]:checked`)];
+        let result = []
+        itemChoices.forEach(element => result.push(element.value))        
+        return result;
+    }
    
+    // универсальный калькулятор цены и калорийности, в том числе с учетом множественного выбора топпингов
     _calculator(choice) { 
-        return this.sizes[this.size][choice] + this.stuffings[this.stuffing][choice] + this.toppings[this.topping][choice];  
+        let toppingSum = 0;
+        for (let value of Object.values(this.topping)){         
+            toppingSum += this.toppings[value][choice]   
+        };
+
+        return this.sizes[this.size][choice] + this.stuffings[this.stuffing][choice] + toppingSum;  
     }
 
+    // метод для вывода результата расчетов на страницу
     showResult(price, calories){
         document.querySelector(price).textContent = this._calculator('price');
         document.querySelector(calories).textContent = this._calculator('calories');
